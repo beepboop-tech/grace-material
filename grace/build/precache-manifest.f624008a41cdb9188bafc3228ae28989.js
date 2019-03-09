95b9575f53b2a35418c76008c22a54efaa792dec
Tomos Evans <tomosamevans@gmail.com>
2019-03-09 21:08:07 +0000

    build

---
+++ grace/build/precache-manifest.f624008a41cdb9188bafc3228ae28989.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "a7aeb941b81aee9935cd",
+    "url": "/static/js/main.c7931d91.chunk.js"
+  },
+  {
+    "revision": "f31b5e61a26efa386b15",
+    "url": "/static/js/2.357eb387.chunk.js"
+  },
+  {
+    "revision": "a7aeb941b81aee9935cd",
+    "url": "/static/css/main.7a3b8648.chunk.css"
+  },
+  {
+    "revision": "e214ce09d6071119366fdbe6ce60c555",
+    "url": "/index.html"
+  }
+];
